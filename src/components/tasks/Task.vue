d4f7bc00f0853cd8e9348dc0a22cc451d3166e35
João P. Assunção <jfernandees002@gmail.com>
2022-02-04 21:00:09 -0300

    In progres

---
+++ src/components/tasks/Task.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-list-item>
+      <template v-slot:default="{ active }">
+        <v-list-item-action>
+          <v-checkbox :input-value="active"></v-checkbox>
+        </v-list-item-action>
+
+        <v-list-item-content>
+          <v-list-item-title>{{task.title}}</v-list-item-title>
+        </v-list-item-content>
+      </template>
+    </v-list-item>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['task']
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Tasks.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="tasklist">
+    <v-col
+      cols="12"
+      sm="6"
+    >
+      <v-text-field
+        v-model="input"
+        label="Add a task"
+        outlined
+        clearable
+        @keyup.enter="handleAddTask"
+      ></v-text-field>
+    </v-col>
+  
+    <div>
+      <v-list flat subheader>
+        <v-list-item-group>
+          
+          <div 
+            v-for="task, index in tasks" 
+            :key="index"
+          >
+            <Task :task="task" />
+          </div>
+
+        </v-list-item-group>
+      </v-list>
+    </div>
+  </div>
+</template>
+
+<script>
+import Task from '../components/tasks/Task.vue'
+  export default {
+    name: 'Home',
+    components: { Task },
+    data() {
+      return {
+        input: null,
+        tasks: [
+          { title: 'Estudar Vue', status: false },
+          { title: 'Commitar o To-do', status: false },
+          { title: 'Estudar React', status: false },
+        ]
+      }
+    },
+    methods: {
+      handleAddTask() {
+        if(this.input) {
+          this.tasks.push({
+            title: this.input,
+            status: false
+          })
+        }
+        this.input = null
+      },
+      handleRemoveTask() {
+        // this.tasks = this.tasks.splice(index, 1)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  div.tasklist {
+    margin-top: 15px;
+  }
+</style>
